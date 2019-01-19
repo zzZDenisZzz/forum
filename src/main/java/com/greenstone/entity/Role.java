0325178ccd0c8b05fea10372f31ce7cdb3dd1c5b
zzZDenysZzz <denys.zazeka@gmail.com>
2019-01-19 17:20:17 +0200

    entity: added role and use

---
+++ src/main/java/com/greenstone/entity/Role.java
@@ -0,0 +1,22 @@
+package com.greenstone.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "roles")
+class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
---
+++ src/main/java/com/greenstone/entity/User.java
@@ -0,0 +1,28 @@
+package com.greenstone.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String username;
+    private String password;
+
+    @Transient
+    String confirmPassword;
+
+    @ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id")
+            , inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
