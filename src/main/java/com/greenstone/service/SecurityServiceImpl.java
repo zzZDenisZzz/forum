e1be78443287f23d4cf4e6ab1d78b6e79a094d8a
zzZDenysZzz <denys.zazeka@gmail.com>
2019-01-20 15:32:20 +0200

    security: made security service

---
+++ src/main/java/com/greenstone/service/SecurityServiceImpl.java
@@ -0,0 +1,45 @@
+package com.greenstone.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public String findLoggedInUsername() {
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        if (userDetails instanceof UserDetails) {
+            return (((UserDetails) userDetails).getUsername());
+        }
+        return null;
+    }
+
+    @Override
+    public void autoLogin(String username, String password) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+        authenticationManager.authenticate(authenticationToken);
+
+        if (authenticationToken.isAuthenticated()){
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            log.debug(String.format("Successfully %s auto logged in",username));
+        }
+    }
+}
