82056a2cf8016b69bae48946ceea459a023d2187
Denys Zazeka <denys.zazeka@gmail.com>
2019-03-15 22:18:56 +0200

    fix SecurityConfig

---
+++ src/main/java/com/greenstone/config/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.greenstone.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
