644658375e5dd21a557cf05439d46af9213e8057
Denys Zazeka <denys.zazeka@gmail.com>
2019-03-15 23:26:48 +0200

    fixed the User.class for correct work

--- src/main/java/com/greenstone/entity/User.java
+++ src/main/java/com/greenstone/entity/User.java
@@ -20,9 +20,4 @@ public class User {
 
     @Transient
     String confirmPassword;
-
-    @ManyToMany
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id")
-            , inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
 }
