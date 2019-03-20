5bd932d29e1d0a6c76f9ea03754c46016ef47511
Denys Zazeka <denys.zazeka@gmail.com>
2019-03-20 15:35:58 +0200

    update UserServiceImpl(method save)

--- src/main/java/com/greenstone/service/UserServiceImpl.java
+++ src/main/java/com/greenstone/service/UserServiceImpl.java
@@ -1,26 +1,36 @@
 package com.greenstone.service;
 
+import com.greenstone.dao.RoleDao;
 import com.greenstone.dao.UserDao;
+import com.greenstone.entity.Role;
 import com.greenstone.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     private UserDao userDao;
+    private RoleDao roleDao;
     private PasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserDao userDao, PasswordEncoder bCryptPasswordEncoder) {
+    public UserServiceImpl(UserDao userDao,RoleDao roleDao, PasswordEncoder bCryptPasswordEncoder) {
         this.userDao = userDao;
+        this.roleDao = roleDao;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleDao.getOne(1L));
+        user.setRoles(roles);
         userDao.save(user);
     }
 
