45798d3748d9e0e295f0200ba2273d6733c413e5
Denys Zazeka <denys.zazeka@gmail.com>
2019-03-20 14:57:36 +0200

    update database.sql added tables

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -5,3 +5,30 @@ create table users
   username text not null,
   password varchar(255)
 );
+
+--- Table: roles
+create table roles
+(
+  id   serial primary key,
+  name varchar(30)
+);
+
+-- Table for mapping user and roles: user_roles
+create table user_roles
+(
+  user_id int not null,
+  role_id int not null,
+
+  foreign key (user_id) references users(id),
+  foreign key (role_id) references roles(id),
+
+  unique (user_id,role_id)
+);
+
+-- Insert data
+insert into users values (1,'Admin','$2a$04$8hYx7aVAiAR.wWE0UsjngeAFyu/dZm8oRRM2lgd26F4SUwOqlqjvu');
+
+insert into roles values(1,'ROLE_USER');
+insert into roles values (2,'ROLE_ADMIN');
+
+insert into user_roles values (1,2);
